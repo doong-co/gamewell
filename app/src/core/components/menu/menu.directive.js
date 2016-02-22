02a7c98d6b864b41bc730adbb7a4753db27506f8
kdq_thomasvo <thomas.vo@weather.com>
2016-02-21 17:42:26 -0800

    added Bluemix dialog

--- app/src/core/components/menu/menu.directive.js
+++ app/src/core/components/menu/menu.directive.js
@@ -15,7 +15,29 @@
     ]);
 
     function mspCoreMenu($route, $rootScope, menuService) {
-      var introStarted = false;
+
+      var spinner = new Spinner({
+          lines: 9 // The number of lines to draw
+        , length: 0 // The length of each line
+        , width: 7 // The line thickness
+        , radius: 27 // The radius of the inner circle
+        , scale: 1.25 // Scales overall size of the spinner
+        , corners: 1 // Corner roundness (0..1)
+        , color: '#fff' // #rgb or #rrggbb or array of colors
+        , opacity: 0.25 // Opacity of the lines
+        , rotate: 71 // The rotation offset
+        , direction: 1 // 1: clockwise, -1: counterclockwise
+        , speed: 1.4 // Rounds per second
+        , trail: 28 // Afterglow percentage
+        , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+        , zIndex: 2e9 // The z-index (defaults to 2000000000)
+        , className: 'spinner' // The CSS class to assign to the spinner
+        , top: '50%' // Top position relative to parent
+        , left: '50%' // Left position relative to parent
+        , shadow: false // Whether to render a shadow
+        , hwaccel: false // Whether to use hardware acceleration
+        , position: 'absolute' // Element positioning
+        });
 
       return {
         templateUrl: viewTemplate,
@@ -29,20 +51,23 @@
           .loadMenu()
           .then(function(menuItems) {
             scope.menuItems = menuItems;
-            setTimeout(function() {
-              $('md-toolbar .md-button[href="#/"]').attr('data-intro','You\'re at the Newsfeed. Scroll to a game to immediately play it.');
-              $('md-toolbar .md-button[href="#/publish_game"]').attr('data-intro','This is where you publish your game.');
-              introStarted || introJs().start();
-              introStarted = true;
-              // debugger;
-            }, 2000);
           });
 
+        
+    
+        // spinner.spin($('form[name="projectForm"]')[0]); 
+        $rootScope.$on('$routeChangeStart', function(e, data) {
+          spinner.spin($('md-toolbar')[0]);
+        });
         $rootScope.$on('$routeChangeSuccess', function(e, data) {
           _.map(scope.menuItems, function(menuItem) {
             menuItem.active = (data.scope.page.pageType === menuItem.pageType);
           })
+          spinner.stop();
         });
+
+        window.kdqMenuInit = true;
+
       }
 
       function viewTemplate() {
